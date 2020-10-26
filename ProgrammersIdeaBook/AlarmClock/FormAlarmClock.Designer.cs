e1ab6f9f502917c8d2f8367a836277e1fdeb1761
Kyle Givler <xvegan88x@gmail.com>
2020-10-25 21:40:41 -0400

    Alarm Clock

--- ProgrammersIdeaBook/AlarmClock/FormAlarmClock.Designer.cs
+++ ProgrammersIdeaBook/AlarmClock/FormAlarmClock.Designer.cs
@@ -31,19 +31,21 @@
             this.components = new System.ComponentModel.Container();
             this.lblCurrentTime = new System.Windows.Forms.Label();
             this.timerSecond = new System.Windows.Forms.Timer(this.components);
-            this.lbltest = new System.Windows.Forms.Label();
             this.lblDay = new System.Windows.Forms.Label();
             this.lblMonth = new System.Windows.Forms.Label();
             this.lblYear = new System.Windows.Forms.Label();
             this.textBoxMonth = new System.Windows.Forms.TextBox();
             this.textBoxDay = new System.Windows.Forms.TextBox();
             this.textBoxYear = new System.Windows.Forms.TextBox();
-            this.btnValidiate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.lblTime = new System.Windows.Forms.Label();
             this.textBoxHour = new System.Windows.Forms.TextBox();
             this.textBoxMinute = new System.Windows.Forms.TextBox();
             this.textBoxSecond = new System.Windows.Forms.TextBox();
             this.comboBoxAmPm = new System.Windows.Forms.ComboBox();
+            this.groupBoxSetDate = new System.Windows.Forms.GroupBox();
+            this.listBoxAlarms = new System.Windows.Forms.ListBox();
+            this.groupBoxSetDate.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblCurrentTime
@@ -62,20 +64,11 @@
             this.timerSecond.Interval = 1000;
             this.timerSecond.Tick += new System.EventHandler(this.timerSecond_Tick);
             // 
-            // lbltest
-            // 
-            this.lbltest.AutoSize = true;
-            this.lbltest.Location = new System.Drawing.Point(30, 409);
-            this.lbltest.Name = "lbltest";
-            this.lbltest.Size = new System.Drawing.Size(26, 15);
-            this.lbltest.TabIndex = 1;
-            this.lbltest.Text = "test";
-            // 
             // lblDay
             // 
             this.lblDay.AutoSize = true;
             this.lblDay.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDay.Location = new System.Drawing.Point(12, 87);
+            this.lblDay.Location = new System.Drawing.Point(6, 62);
             this.lblDay.Name = "lblDay";
             this.lblDay.Size = new System.Drawing.Size(54, 30);
             this.lblDay.TabIndex = 0;
@@ -85,7 +78,7 @@
             // 
             this.lblMonth.AutoSize = true;
             this.lblMonth.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblMonth.Location = new System.Drawing.Point(12, 57);
+            this.lblMonth.Location = new System.Drawing.Point(6, 32);
             this.lblMonth.Name = "lblMonth";
             this.lblMonth.Size = new System.Drawing.Size(80, 30);
             this.lblMonth.TabIndex = 0;
@@ -95,7 +88,7 @@
             // 
             this.lblYear.AutoSize = true;
             this.lblYear.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblYear.Location = new System.Drawing.Point(12, 117);
+            this.lblYear.Location = new System.Drawing.Point(6, 92);
             this.lblYear.Name = "lblYear";
             this.lblYear.Size = new System.Drawing.Size(57, 30);
             this.lblYear.TabIndex = 0;
@@ -103,40 +96,40 @@
             // 
             // textBoxMonth
             // 
-            this.textBoxMonth.Location = new System.Drawing.Point(99, 66);
+            this.textBoxMonth.Location = new System.Drawing.Point(93, 41);
             this.textBoxMonth.Name = "textBoxMonth";
-            this.textBoxMonth.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMonth.Size = new System.Drawing.Size(43, 23);
             this.textBoxMonth.TabIndex = 2;
             // 
             // textBoxDay
             // 
-            this.textBoxDay.Location = new System.Drawing.Point(99, 96);
+            this.textBoxDay.Location = new System.Drawing.Point(93, 71);
             this.textBoxDay.Name = "textBoxDay";
-            this.textBoxDay.Size = new System.Drawing.Size(100, 23);
+            this.textBoxDay.Size = new System.Drawing.Size(43, 23);
             this.textBoxDay.TabIndex = 3;
             // 
             // textBoxYear
             // 
-            this.textBoxYear.Location = new System.Drawing.Point(99, 126);
+            this.textBoxYear.Location = new System.Drawing.Point(93, 101);
             this.textBoxYear.Name = "textBoxYear";
-            this.textBoxYear.Size = new System.Drawing.Size(100, 23);
+            this.textBoxYear.Size = new System.Drawing.Size(43, 23);
             this.textBoxYear.TabIndex = 3;
             // 
-            // btnValidiate
+            // btnAdd
             // 
-            this.btnValidiate.Location = new System.Drawing.Point(62, 405);
-            this.btnValidiate.Name = "btnValidiate";
-            this.btnValidiate.Size = new System.Drawing.Size(75, 23);
-            this.btnValidiate.TabIndex = 4;
-            this.btnValidiate.Text = "Validate";
-            this.btnValidiate.UseVisualStyleBackColor = true;
-            this.btnValidiate.Click += new System.EventHandler(this.btnValidiate_Click);
+            this.btnAdd.Location = new System.Drawing.Point(91, 201);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add Alarm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnValidiate_Click);
             // 
             // lblTime
             // 
             this.lblTime.AutoSize = true;
             this.lblTime.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTime.Location = new System.Drawing.Point(9, 177);
+            this.lblTime.Location = new System.Drawing.Point(6, 143);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(63, 30);
             this.lblTime.TabIndex = 0;
@@ -144,21 +137,21 @@
             // 
             // textBoxHour
             // 
-            this.textBoxHour.Location = new System.Drawing.Point(78, 184);
+            this.textBoxHour.Location = new System.Drawing.Point(75, 150);
             this.textBoxHour.Name = "textBoxHour";
             this.textBoxHour.Size = new System.Drawing.Size(29, 23);
             this.textBoxHour.TabIndex = 3;
             // 
             // textBoxMinute
             // 
-            this.textBoxMinute.Location = new System.Drawing.Point(113, 184);
+            this.textBoxMinute.Location = new System.Drawing.Point(110, 150);
             this.textBoxMinute.Name = "textBoxMinute";
             this.textBoxMinute.Size = new System.Drawing.Size(29, 23);
             this.textBoxMinute.TabIndex = 3;
             // 
             // textBoxSecond
             // 
-            this.textBoxSecond.Location = new System.Drawing.Point(148, 184);
+            this.textBoxSecond.Location = new System.Drawing.Point(145, 150);
             this.textBoxSecond.Name = "textBoxSecond";
             this.textBoxSecond.Size = new System.Drawing.Size(29, 23);
             this.textBoxSecond.TabIndex = 3;
@@ -166,32 +159,53 @@
             // comboBoxAmPm
             // 
             this.comboBoxAmPm.FormattingEnabled = true;
-            this.comboBoxAmPm.Location = new System.Drawing.Point(183, 184);
+            this.comboBoxAmPm.Location = new System.Drawing.Point(180, 150);
             this.comboBoxAmPm.Name = "comboBoxAmPm";
             this.comboBoxAmPm.Size = new System.Drawing.Size(55, 23);
             this.comboBoxAmPm.TabIndex = 5;
+            // 
+            // groupBoxSetDate
+            // 
+            this.groupBoxSetDate.Controls.Add(this.lblMonth);
+            this.groupBoxSetDate.Controls.Add(this.btnAdd);
+            this.groupBoxSetDate.Controls.Add(this.comboBoxAmPm);
+            this.groupBoxSetDate.Controls.Add(this.lblDay);
+            this.groupBoxSetDate.Controls.Add(this.lblYear);
+            this.groupBoxSetDate.Controls.Add(this.textBoxSecond);
+            this.groupBoxSetDate.Controls.Add(this.textBoxMonth);
+            this.groupBoxSetDate.Controls.Add(this.textBoxMinute);
+            this.groupBoxSetDate.Controls.Add(this.textBoxDay);
+            this.groupBoxSetDate.Controls.Add(this.textBoxHour);
+            this.groupBoxSetDate.Controls.Add(this.textBoxYear);
+            this.groupBoxSetDate.Controls.Add(this.lblTime);
+            this.groupBoxSetDate.Location = new System.Drawing.Point(13, 82);
+            this.groupBoxSetDate.Name = "groupBoxSetDate";
+            this.groupBoxSetDate.Size = new System.Drawing.Size(257, 245);
+            this.groupBoxSetDate.TabIndex = 6;
+            this.groupBoxSetDate.TabStop = false;
+            this.groupBoxSetDate.Text = "Set Date";
+            // 
+            // listBoxAlarms
+            // 
+            this.listBoxAlarms.FormattingEnabled = true;
+            this.listBoxAlarms.ItemHeight = 15;
+            this.listBoxAlarms.Location = new System.Drawing.Point(292, 82);
+            this.listBoxAlarms.Name = "listBoxAlarms";
+            this.listBoxAlarms.Size = new System.Drawing.Size(274, 244);
+            this.listBoxAlarms.TabIndex = 7;
             // 
             // FormAlarmClock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBoxAmPm);
-            this.Controls.Add(this.btnValidiate);
-            this.Controls.Add(this.textBoxSecond);
-            this.Controls.Add(this.textBoxMinute);
-            this.Controls.Add(this.textBoxHour);
-            this.Controls.Add(this.textBoxYear);
-            this.Controls.Add(this.textBoxDay);
-            this.Controls.Add(this.textBoxMonth);
-            this.Controls.Add(this.lbltest);
-            this.Controls.Add(this.lblTime);
-            this.Controls.Add(this.lblYear);
-            this.Controls.Add(this.lblMonth);
-            this.Controls.Add(this.lblDay);
+            this.ClientSize = new System.Drawing.Size(780, 361);
+            this.Controls.Add(this.listBoxAlarms);
+            this.Controls.Add(this.groupBoxSetDate);
             this.Controls.Add(this.lblCurrentTime);
             this.Name = "FormAlarmClock";
             this.Text = "Alarm Clock";
+            this.groupBoxSetDate.ResumeLayout(false);
+            this.groupBoxSetDate.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,19 +215,20 @@
 
         private System.Windows.Forms.Label lblCurrentTime;
         private System.Windows.Forms.Timer timerSecond;
-        private System.Windows.Forms.Label lbltest;
         private System.Windows.Forms.Label lblDay;
         private System.Windows.Forms.Label lblMonth;
         private System.Windows.Forms.Label lblYear;
         private System.Windows.Forms.TextBox textBoxMonth;
         private System.Windows.Forms.TextBox textBoxDay;
         private System.Windows.Forms.TextBox textBoxYear;
-        private System.Windows.Forms.Button btnValidiate;
+        private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.TextBox textBoxHour;
         private System.Windows.Forms.TextBox textBoxMinute;
         private System.Windows.Forms.TextBox textBoxSecond;
         private System.Windows.Forms.ComboBox comboBoxAmPm;
+        private System.Windows.Forms.GroupBox groupBoxSetDate;
+        private System.Windows.Forms.ListBox listBoxAlarms;
     }
 }
 
--- ProgrammersIdeaBook/AlarmClock/FormAlarmClock.cs
+++ ProgrammersIdeaBook/AlarmClock/FormAlarmClock.cs
@@ -37,23 +37,31 @@ namespace AlarmClock
             }
 
             textBoxHour.Text = hour.ToString();
+
             textBoxMinute.Text = now.Minute.ToString();
             if(textBoxMinute.Text.Length != 2)
             {
                 textBoxMinute.Text = $"0{textBoxMinute.Text}";
             }
+
             textBoxSecond.Text = now.Second.ToString();
+            if (textBoxSecond.Text.Length != 2)
+            {
+                textBoxSecond.Text = $"0{textBoxSecond.Text}";
+            }
         }
 
         private void timerSecond_Tick(object sender, EventArgs e)
         {
             lblCurrentTime.Text = $"Current date and time: {DateTime.Now}";
-            lbltest.Text = DateTime.Now.Date.Day.ToString();
         }
 
         private void btnValidiate_Click(object sender, EventArgs e)
         {
-            IsValidForm();
+            if (!IsValidForm())
+            {
+                MessageBox.Show("Please enter a valid date and time.", "Date Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private bool IsValidForm()
@@ -96,18 +104,19 @@ namespace AlarmClock
                 valid = false;
             }
 
+            if (comboBoxAmPm.SelectedIndex == 1)
+            {
+                hour += 12;
+            }
+
+            DateTime date = new DateTime();
             try
             {
-                DateTime date = new DateTime(year, month, day, hour, minute, second);
+                date = new DateTime(year, month, day, hour, minute, second);
             }
             catch (ArgumentOutOfRangeException e)
             {
                 valid = false;
-            }
-
-            if (!valid)
-            {
-                MessageBox.Show("Nope!");
             }
 
             return valid;
