d68f948768897e15d6f055b95113be25f53bd022
Kyle Givler <xvegan88x@gmail.com>
2021-04-11 22:11:39 -0400

    Add license block

---
+++ ProgrammersIdeaBook/RSSFeedCreator/RssReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+using System.Linq;
+using System.Text;
+
+namespace RSSFeedCreator
+{
+    // Ignore this for testing...
+    public class RssReader
+    {
+        public void ReadXml()
+        {
+            using (XmlReader reader = XmlReader.Create("rss.xml"))
+            {
+                reader.MoveToContent();
+
+                while(reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element when reader.Name == "channel":
+                            Console.WriteLine(reader.Name);
+                            break;
+                        case XmlNodeType.Text:
+                            Console.WriteLine(reader.Value);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }    
+        }
+    }
+}
