69c130738e76eb15009976969c57908bc92c3793
Kyle Givler <xvegan88x@gmail.com>
2021-11-26 09:31:08 -0500

    Add Fraction Razor Pages

---
+++ ProgrammersIdeaBook/FractionCalcRazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using FractionCalcRazorPages.Helpers;
+using FractionCalcRazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FractionCalcRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public List<string> Fractions { get; set; } = new List<string>();
+
+        [BindProperty]
+        public string Option { get; set; } = string.Empty;
+
+        public string Result { get; set; } = String.Empty;
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            var models = new List<Fraction>();
+            foreach (var fraction in Fractions)
+            {
+                if (FractionHelper.TryParseFraction(fraction, out Fraction res))
+                {
+                    models.Add(res);
+                } 
+                else
+                {
+                    throw new Exception("Invalid Fraction!");
+                }
+            }
+
+            Fraction output = models[0];
+            for(int i = 1; i< models.Count; i++)
+            {
+                switch (Option)
+                {
+                    case "Add":
+                        output += models[i];
+                        break;
+                    case "Subtract":
+                        output -= models[i];
+                        break;
+                    case "Divide":
+                        output /= models[i];
+                        break;
+                    case "Multiply":
+                        output *= models[i];
+                        break;
+                    case "LCDGCD":
+                        var dens = models.Select(x => x.Denominator);
+                        Result = $"LCD: {FractionHelper.FindGreatestCommonFactor(dens.ToArray())} GCD: {FractionHelper.FindGreatestCommonFactor(dens.ToArray())}";
+                        break;
+                    default:
+                        Result = "Something went wrong :(";
+                        break;
+                }
+            }
+            if (Option != "LCDGCD")
+            {
+                Result = output.ToString();
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ ProgrammersIdeaBook/FractionCalcRazorPages/wwwroot/js/validateFraction.js
@@ -0,0 +1,17 @@
+﻿const validateFraction = (fraction) => {
+    const parts = fraction.split('/');
+
+    if (parts.length == 0 || parts.length > 2) {
+        return false;
+    }
+
+    if (!Number.isInteger(parseInt(parts[0]))) {
+        return false;
+    }
+
+    if (parts.length == 2 && !Number.isInteger(parseInt(parts[1]))) {
+        return false;
+    }
+
+    return true;
+};
