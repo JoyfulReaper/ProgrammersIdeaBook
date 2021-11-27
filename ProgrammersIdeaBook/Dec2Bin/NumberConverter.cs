f072d739e211c9ce13d445ae6a5b7b175176a1e4
Kyle Givler <xvegan88x@gmail.com>
2021-11-27 17:55:23 -0500

    Pull out logic

---
+++ ProgrammersIdeaBook/Dec2Bin/NumberConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace Dec2Bin
+{
+    public static class NumberConverter
+    {
+        public static string DecToBin(int number)
+        {
+            string result = string.Empty;
+            while (number > 0)
+            {
+                result = number % 2 + result;
+                number /= 2;
+            }
+
+            return result == "" ? "0" : result;
+        }
+
+        public static int BinToDec(string number)
+        {
+            if (!int.TryParse(number, out _))
+            {
+                throw new ArgumentException(nameof(number) + " could not be converted to an integer!");
+            }
+
+            int result = 0;
+            int place = 0;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                var curr = int.Parse(number[i].ToString());
+                if (curr == 1)
+                {
+                    result = result + (int)Math.Pow(2, place);
+                }
+                place++;
+            }
+
+            return result;
+        }
+    }
+}
--- ProgrammersIdeaBook/Dec2Bin/Program.cs
+++ ProgrammersIdeaBook/Dec2Bin/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("This application converts from decimal to binary and back again for the Programmers Idea Book Project.");
+﻿using Dec2Bin;
+
+Console.WriteLine("This application converts from decimal to binary and back again for the Programmers Idea Book Project.");
 Console.Write("Enter Decimal Number: ");
 var input = Console.ReadLine();
 
@@ -8,44 +10,10 @@ if(!int.TryParse(input, out var number))
     Environment.Exit(1);
 }
 
-var bin = DecToBin(number);
-var dec = BinToDec(bin);
+var bin = NumberConverter.DecToBin(number);
+var dec = NumberConverter.BinToDec(bin);
 
 Console.WriteLine();
 Console.WriteLine("Result:");
 Console.WriteLine(bin);
 Console.WriteLine(dec);
-
-static string DecToBin(int number)
-{
-    string result = string.Empty;
-    while (number > 0)
-    {
-        result = number % 2 + result;
-        number /= 2;
-    }
-
-    return result;
-}
-
-static int BinToDec(string number)
-{
-    if(!int.TryParse(number, out _))
-    {
-        throw new ArgumentException(nameof(number) + " could not be converted to an integer!");
-    }
-
-    int result = 0;
-    int place = 0;
-    for(int i = number.Length -1; i >= 0; i--)
-    {
-        var curr = int.Parse(number[i].ToString());
-        if (curr == 1)
-        {
-            result = result + (int)Math.Pow(2, place);
-        }
-        place++;
-    }
-
-    return result;
-}
